34616fc9f49a746b393f5b8b7c24f8b720c1ac1a
Chris Andreotti <candreotti@penguinrandomhouse.com>
2018-12-02 11:53:10 -0500

    Adds state to SearchBar, captures input in state and writes value back to input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    state = {term: ''}
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="ui field">
                         <label>Image Search</label>
-                        <input type="text" onChange={(event) => console.log(event.target.value)} />
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={(e) => this.setState({term: e.target.value})} 
+                        />
                     </div>
                 </form>
             </div>
