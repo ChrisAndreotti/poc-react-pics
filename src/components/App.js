667598a7bac0c88c320b8f77f6c409bcd733c2e6
Chris Andreotti <candreotti@penguinrandomhouse.com>
2018-12-02 14:37:09 -0500

    Add async/await request to Unsplash image search API

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_UNSPLASH_SECRET_ACCESS_KEY : process.env.REACT_APP_DEV_UNSPLASH_SECRET_ACCESS_KEY;
+const apiSecretKey = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_UNSPLASH_SECRET_ACCESS_KEY : process.env.REACT_APP_DEV_UNSPLASH_SECRET_ACCESS_KEY;
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
-                Authorization: `Client-ID ${apiUrl}`
+                Authorization: `Client-ID ${apiSecretKey}`
             }
         });
+        console.log(response.data.results);
     }
 
     render() {
