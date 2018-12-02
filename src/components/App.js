719cb0ff9f7e8e37cbd88d5fad89f735c210dc31
Chris Andreotti <candreotti@penguinrandomhouse.com>
2018-12-02 15:00:38 -0500

    Sets state after async request to Unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,24 @@ const apiSecretKey = process.env.NODE_ENV === 'production' ? process.env.REACT_A
 
 class App extends React.Component {
 
-    async onSearchSubmit(term) {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: `Client-ID ${apiSecretKey}`
             }
         });
-        console.log(response.data.results);
+
+        this.setState({images: response.data.results});
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
